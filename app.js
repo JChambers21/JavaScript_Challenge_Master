5fd3237eda0973951b89b3c38f604b29c0b894d3
JChambers21 <joshchambers2112@gmail.com>
2022-11-24 22:52:01 +0000

    Final Changes
    The final version of the interview challenge

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const path = require('path')
-const stocks = require('./stocks')
+const express = require('express') // variable to give access to express module
+const path = require('path') // variable to give access to path module
+const stocks = require('./stocks') // variable to give to access stocks.js module
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+const app = express() // call the object to create an express application (can then access properties and functions of the application object)
 
+//Setting up middleware for application
+app.use(express.static(path.join(__dirname, 'static'))) 
+
+//function to call the stocks.getStocks function to display available stocks
+//returns an array of each stock, a promise
 app.get('/stocks', async (req, res) => {
   const stockSymbols = await stocks.getStocks()
+  console.log(stockSymbols); //checking for call
   res.send({ stockSymbols })
 })
 
+//function to call the stocks.getStockPoints function to display the data for an individual stock
 app.get('/stocks/:symbol', async (req, res) => {
   const { params: { symbol } } = req
   const data = await stocks.getStockPoints(symbol, new Date())
